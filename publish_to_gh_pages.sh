b659ca2e1544f5585cb7e53a196508403aef312e
rowan <rowan@rowan.website>
2019-04-05 15:12:12 -0400

    Add push to the script

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -18,7 +18,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -28,3 +28,6 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Pushing changes to gh-pages"
+git push origin gh-pages
